25e43c230ef3f1c128b9e1842cdb04079f2aab75
Giovanni Delgado <giovanniyatze@gmail.com>
2021-10-08 02:56:06 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+require('custom-env').env(true)
+
+const port = process.env.port;
+const router = require('.src/router/router');
+
+// Create an express server and a GraphQL endpoint
+var app = express();
+app.use('/', router);
+
+var server = app.listen(port, () => {
+	console.log(`App running on port ${port}.`);
+});
